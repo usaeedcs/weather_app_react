84e2b43c548d5c28a423798853ec82a5b0b3fc5e
usaeedcs <usman.s20005@gmail.com>
2024-01-15 14:09:29 -0800

    Complete weather app

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': //taken from GeoDB Cities Rapid API
+		'X-RapidAPI-Host': //taken from GeoDB Cities Rapid API
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY =  // enter your own API key
